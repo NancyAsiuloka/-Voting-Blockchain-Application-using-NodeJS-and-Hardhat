50d51e73afe6d9de4638786322e984e6bde5f512
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-23 18:27:00 +0100

    required some modules

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// SERVER
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const fileUpload = require('express-fileupload');
+app.use(
+    fileUpload({
+        extended:true
+    })
+)
+app.use(express.static(__dirname));
+app.use(express.json());
+const path = require("path");
+const ethers = require('ethers');
+
+var port = 3000;
