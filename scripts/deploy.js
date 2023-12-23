ca8e63955f52634b88b73e194c44683a1db8a23c
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-23 19:11:33 +0100

    made some changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,6 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
     value: lockedAmount,
   });
@@ -19,9 +8,7 @@ async function main() {
   await lock.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `Contract address : ${lock.target}`
   );
 }
 
