103439a91cce525e03c3ddd0d837d4145cf707aa
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-25 11:53:42 +0100

    made some changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const { ethers } = require("ethers");
 async function main() {
   try {
     // Connect to the local Ethereum network
-    const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+    const provider = new ethers.providers.JsonRpcProvider("https://volta-rpc.energyweb.org/");
 
     // Set the default wallet with a private key (replace with your private key)
     const privateKey = "0x300de365cb6cb4ecdae7aaa6d15c5ef0107731fcc9c8d848833fcf6c07c987ff";
